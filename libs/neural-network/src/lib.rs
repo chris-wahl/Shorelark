d0e47fef48f17c8e2e8bbf8e5961b40cd409aed0
Christopher Wahl <80052692+chris-wahl@users.noreply.github.com>
2022-10-10 12:47:03 -0400

    Init basic network structs

---
+++ libs/neural-network/src/lib.rs
@@ -0,0 +1,44 @@
+use std::intrinsics::bitreverse;
+
+pub struct Network {
+    layers: Vec<Layer>,
+}
+
+struct Layer {
+    neurons: Vec<Neuron>,
+}
+
+struct Neuron {
+    bias: f32,
+    weights: Vec<f32>,
+}
+
+impl Network {
+    pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        return self.layers
+            .iter()
+            .fold(inputs, |inputs, layer| layer.propagate(inputs));
+    }
+}
+
+impl Layer {
+    fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        return self.neurons
+            .iter()
+            .map(|neuron| neuron.propagate(&inputs))
+            .collect();
+    }
+}
+
+impl Neuron {
+    fn propagate(&self, inputs: &[f32]) -> f32 {
+        assert_eq!(inputs.len(), self.weights.len());
+
+        let output = inputs
+            .iter()
+            .zip(&self.weights)
+            .map(|(input, weight)| input * weight)
+            .sum::<f32>();
+        return (self.bias + output).max(0.0)
+    }
+}
