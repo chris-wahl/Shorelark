037ba69f84f3001f9af5df34752e32158cea7a68
Christopher Wahl <80052692+chris-wahl@users.noreply.github.com>
2022-10-10 20:56:02 -0400

    Adding in `canvas` printing

--- www/index.js
+++ www/index.js
@@ -1,8 +1,26 @@
 import * as sim from "lib-simulation-wasm";
 
 const simulation = new sim.Simulation();
+const world = simulation.world();
 
-const body = document.getElementsByTagName("body")[0];
-const new_tag = document.createElement('p');
-new_tag.textContent = "Who's that dog? " + sim.whos_that_dog() + "!";
-body.append(new_tag);
+const viewport = document.getElementById('viewport');
+const viewport_scale = window.devicePixelRatio || 1;
+const [viewport_width, viewport_height] = [viewport.width, viewport.height];
+viewport.width = viewport_width * viewport_scale;
+viewport.height = viewport_height * viewport_scale;
+viewport.style.width = viewport_width + 'px';
+viewport.style.height = viewport_height + 'px';
+
+
+const context = viewport.getContext('2d');
+context.fillStyle = 'rgb(0, 0, 0)';
+
+for (const animal of simulation.world().animals) {
+    context.fillRect(
+        animal.x * viewport_width,
+        animal.y * viewport_height,
+        15,
+        15,
+    );
+}
+
