cdf1717ad4a204ded1c8919d7f715cbf159c7e93
Christopher Wahl <80052692+chris-wahl@users.noreply.github.com>
2022-10-11 10:15:23 -0400

    Applied Neural Network to birds

--- libs/simulation/src/animal.rs
+++ libs/simulation/src/animal.rs
@@ -1,4 +1,4 @@
-use crate::{na, Rng, RngCore};
+use crate::*;
 
 
 #[derive(Debug)]
@@ -6,14 +6,54 @@ pub struct Animal {
     pub(crate) position: na::Point2<f32>,
     pub(crate) rotation: na::Rotation2<f32>,
     pub(crate) speed: f32,
+
+    pub (crate) eye: Eye,
+    pub (crate) brain: nn::Network,
 }
 
 impl Animal {
     pub fn random(rng: &mut dyn RngCore) -> Self {
+        let eye = Eye::default();
+        let brain = nn::Network::random(
+            rng,
+            &[
+                // The Input Layer
+                //
+                // Because our eye returns Vec<f32>, and our neural
+                // network works on Vec<f32>, we can pass-through
+                // numbers from eye into the neural network directly.
+                nn::LayerTopology {
+                    neurons: eye.cells(),
+                },
+
+                // The Hidden Layer
+                //
+                // There is no best answer as to "how many neurons
+                // the hidden layer should contain" (or how many
+                // hidden layers there should be, even - there could
+                // be zero, one, two or more!).
+                //
+                // The rule of thumb is to start with a single hidden
+                // layer that has somewhat more neurons that the input
+                // layer, and see how well the network performs.
+                nn::LayerTopology {
+                    neurons: 2 * eye.cells(),
+                },
+
+                // The Output Layer
+                //
+                // Since the brain will control our bird's speed and
+                // rotation, this gives us two numbers = two neurons.
+                nn::LayerTopology { neurons: 2 },
+            ]
+        );
+
         return Self {
             position: rng.gen(),
             rotation: rng.gen(),
             speed: 0.002,
+            eye,
+            brain,
         };
     }
 
--- libs/simulation/src/lib.rs
+++ libs/simulation/src/lib.rs
@@ -1,8 +1,13 @@
+use std::f32::consts::FRAC_PI_2;
+
 use nalgebra as na;
 use rand::{Rng, RngCore};
 
+use lib_neural_network as nn;
+
 pub use crate::{
     animal::Animal,
+    eye::Eye,
     food::Food,
     world::World,
 };
@@ -11,6 +16,15 @@ mod animal;
 mod eye;
 mod food;
 mod world;
+
+const SPEED_MIN: f32 = 0.001;
+// Non-zero speed prevents bird from being stuck in one place
+const SPEED_MAX: f32 = 0.005;
+// Prevent the bird from approaching the speed of causality `c`
+const SPEED_ACCEL: f32 = 0.2;
+// Prevent infinite accelerations in speed
+const ROTATION_ACCEL: f32 = FRAC_PI_2; // And in rotation
+
 
 pub struct Simulation {
     world: World,
@@ -27,15 +41,8 @@ impl Simulation {
 
     pub fn step(&mut self, rng: &mut dyn RngCore) {
         self.process_collisions(rng);
+        self.process_brains();
         self.process_movements();
-    }
-
-    fn process_movements(&mut self) {
-        for animal in &mut self.world.animals {
-            animal.position += animal.rotation * na::Vector2::new(animal.speed, 0.0);
-            animal.position.x = na::wrap(animal.position.x, 0.0, 1.0);
-            animal.position.y = na::wrap(animal.position.y, 0.0, 1.0);
-        }
     }
 
     fn process_collisions(&mut self, rng: &mut dyn RngCore) {
@@ -50,7 +57,32 @@ impl Simulation {
                     food.position = rng.gen();
                 }
             }
+        }
+    }
 
+    fn process_brains(&mut self) {
+        for animal in &mut self.world.animals {
+            let vision = animal.eye.process_vision(
+                animal.position,
+                animal.rotation,
+                &self.world.foods,
+            );
+
+            let response = animal.brain.propagate(vision);
+            let speed_acceleration = response[0].clamp(-SPEED_ACCEL, SPEED_ACCEL);
+            let rotation_acceleration = response[1].clamp(-ROTATION_ACCEL, ROTATION_ACCEL);
+
+            animal.speed = (animal.speed + speed_acceleration).clamp(SPEED_MIN, SPEED_MAX);
+            animal.rotation = na::Rotation2::new(animal.rotation.angle() + rotation_acceleration);
+            // rotation is already wrapped from [0, 2π]
+        }
+    }
+
+    fn process_movements(&mut self) {
+        for animal in &mut self.world.animals {
+            animal.position += animal.rotation * na::Vector2::new(animal.speed, 0.0);
+            animal.position.x = na::wrap(animal.position.x, 0.0, 1.0);
+            animal.position.y = na::wrap(animal.position.y, 0.0, 1.0);
         }
     }
 }
